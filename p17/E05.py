b403b1a0d6a5ecd8bae698db05a5a7c1d74c31de
NuggetReckt <corto.morrow@gmail.com>
2022-01-26 09:24:59 +0100

    E05 fixed + E01 p18

--- p17/E05.py
+++ p17/E05.py
@@ -1,6 +1,10 @@
 nb = int(input("Rentrez un nombre : "))
 
 if nb % 2 != 0 and nb % 3 != 0:
-    print("Le nombre n'est pas divisible par 2 et 3")
+    print("Le nombre n'est pas divisible par 2 et par 3")
+elif nb % 2 != 0:
+    print("Le nombre n'est pas divisible par 2")
+elif nb % 3 != 0:
+    print("Le nombre n'est pas divisible par 3")
 else:
     print("Le nombre est divisble par 2 et par 3")
---
+++ p18/E01.py
@@ -0,0 +1,3 @@
+nb = int(input("Rentrez un nombre : "))
+
+for k in range(nb):
