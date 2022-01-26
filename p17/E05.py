4eb15dbba8854ca4fffa6735c903f077e53bdd71
NuggetReckt <corto.morrow@gmail.com>
2022-01-26 09:08:36 +0100

    E05

---
+++ p17/E05.py
@@ -0,0 +1,6 @@
+nb = int(input("Rentrez un nombre : "))
+
+if nb % 2 != 0 and nb % 3 != 0:
+    print("Le nombre n'est pas divisible par 2 et 3")
+else:
+    print("Le nombre est divisble par 2 et par 3")
